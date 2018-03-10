a1027095accad39680d9b7289dafcfb0ac465a86
Brita Ramsay <britaramsay@gmail.com>
2018-03-10 15:41:28 -0500

    Countdown displayed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,38 @@
-
-
+var clockRunning = false;
 
 $("#play").on("click", function() {
     $("#play").hide();
     $("<div><h3>This is a question?</h3></div>").appendTo("#main-section");
+
+    start();
+    //showQuestion();
 })
+
+var time = 20;
+
+function start(){
+    if(!clockRunning) {
+        var countdown = setInterval(count, 1000);
+        clockRunning = true;
+    }
+}
+
+function count(){
+    console.log("count");
+    time--;
+
+    if (time <= 0) {
+        alert("Out of time")
+    }
+    else
+        $("#countdown").html(time);
+}
+
+function stop() {
+    clearInterval(countdown);
+    clockRunning = false;
+}
+
+function showQuestion(){
+    $("#question").html("questions[i]");
+}
